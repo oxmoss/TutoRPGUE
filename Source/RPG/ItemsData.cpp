448deae55a6fcfcc4d24f8de1fa6449ecfe08c04
oxmoss <hassan.bellounis@gmail.com>
2018-01-20 17:35:27 +0100

    from VS

---
+++ Source/RPG/ItemsData.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ItemsData.h"
+
+
+// Sets default values
+AItemsData::AItemsData()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AItemsData::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AItemsData::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/RPG/MyDataTable.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "MyDataTable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API UMyDataTable : public UDataTable
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
